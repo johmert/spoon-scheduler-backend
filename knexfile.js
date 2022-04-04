7cd2fcec34d563af456b69de78a0e6df65b6f337
johmert <johmert@users.noreply.github.com>
2022-04-04 15:58:26 -0700

    initial user(for tests) added

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,8 @@ module.exports = {
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   }
 };
---
+++ src/db/seeds/00-users.js
@@ -0,0 +1,13 @@
+const initialUser = {
+  user_id: 1,
+  username: "testUser",
+  password: "admin"
+}
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("users").insert(initialUser)
+    });
+}
