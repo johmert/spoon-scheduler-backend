c74e80c0dc7728e5199786e71794a71ad02deb97
johmert <johmert@users.noreply.github.com>
2022-04-03 17:55:45 -0700

    error setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
+const cors = require("cors");
+const errorHandler = require("./errors/errorHandler");
 const express = require("express");
+const notFound = require("./errors/notFound");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ src/errors/notFound.js
@@ -0,0 +1,8 @@
+function notFound(req, res, next) {
+    next({
+        status: 404,
+        message: `The route ${req.path} does not exist!`
+    });
+}
+
+module.exports = notFound;
